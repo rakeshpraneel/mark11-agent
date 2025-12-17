50b9e9edd65894c96811d9fbc0909ea6c798c7a6
Rakesh <rakeshpraneel@gmail.com>
2025-12-17 08:35:01 +0530

    Updated DB url variable

--- app/core/settings.py
+++ app/core/settings.py
@@ -4,7 +4,7 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file='.env', validate_default=False)
     GOOGLE_API_KEY: str = ""
     APP_NAME: str = "sauluh"
-    DB_URL: str = ""
+    DATABASE_URL: str = ""
     QDRANT_CLUSTER: str =""
     QDRANT_API_KEY: str =""
     SERVER_SIDE_CALL: str = ""
